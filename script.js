186a10677ec29ad73fcfc7c78cdb34dde6d3679d
Kenneth <kennethgonzalez067@gmail.com>
2023-01-24 21:59:37 -0500

    work

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function getInput(){
     let input = document.getElementById("input").value;
     
     if(input.toLowerCase() == "home" || input.toLowerCase() == "1"){
-        document.location.pathname = "./index.html";
+        document.location.pathname = "/index.html";
     }
     else if(input.toLowerCase() == "about me"  || input.toLowerCase() == "2"){
-        document.location.pathname = "./htmlFiles/aboutme.html";
+        document.location.pathname = "/htmlFiles/aboutme.html";
     }else if(input.toLowerCase() == "socials" || input.toLowerCase() == "3"){
-        document.location.pathname = "./htmlFiles/socials.html";
+        document.location.pathname = "/htmlFiles/socials.html";
     }else if(input.toLowerCase() == "hobbies" || input.toLowerCase() == "4"){
-        document.location.pathname = "./htmlFiles/hobbies.html"
+        document.location.pathname = "/htmlFiles/hobbies.html"
     }
 }
 
